99ac264eca3e8a9f2defc5b0f348e36384f264c5
sslipushenko <slipushenko@gmail.com>
2016-12-01 17:31:22 +0200

    fix test.lua

--- test.lua
+++ test.lua
@@ -46,5 +46,3 @@ end)
 
 m.client:connect(m.MQTT.server, m.MQTT.port, 0, function(c) print('c') end, function(c,r) print('e',r) end)
 
-_view=function() local _line if file.open("secrets.lua","r") then print("--FileView start") repeat _line = file.readline() if (_line~=nil) then print(string.sub(_line,1,-2)) end until _line==nil file.close() print("--FileView done.") else
-print("\r--FileView error: can't open file") end end _view() _view=nil
