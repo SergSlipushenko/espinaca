32fb97ce583febf941eb738ce293fd4c0a8fd273
sslipushenko <slipushenko@gmail.com>
2018-01-14 17:25:10 +0200

    The very final cut

--- net/wsconsole.lua
+++ net/wsconsole.lua
@@ -1,18 +1,20 @@
 return function(on_connect)
-    local cfg = ldfile('main_cfg.lua') or {}
-    local service = cfg.service == nil and NODEID or cfg.service
-    cfg = nil
-    ldfile('wificon.lua'):start(function()
-        local ws = ldfile('wscon.lua')
-        ws:connect(function()   
-            ws.on_receive = function(_, msg, _)
-                node.input(msg)
-            end
-            node.output(function(result)
-                if ws then ws:buffered_send(result) end
-            end, 1)
-            if on_connect then on_connect(); on_connect=nil end
-            uart.write(0, 'console connected\n')
+    local setup = function(ws)
+        ws.on_receive = function(_, msg, _)
+            node.input(msg)
+        end
+        node.output(function(result)
+            if ws then ws:buffered_send(result) end
+        end, 1)
+        if on_connect then on_connect(); on_connect=nil end
+        uart.write(0, 'console connected\n')
+    end
+    if (nt or {}).ws then 
+        setup(nt.ws)
+    else
+        ldfile('wificon.lua'):start(function()
+            local ws = ldfile('wscon.lua')
+            ws:connect(function() setup(ws) end)
         end)
-    end)
+    end
 end
