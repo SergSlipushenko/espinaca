4e492c149779d88958928ddee0d9c6f034ff0555
sslipushenko <slipushenko@gmail.com>
2018-01-14 18:23:39 +0200

    update flash commands

--- notebook.lua
+++ notebook.lua
@@ -1,9 +1,11 @@
 require 'commons'
-ftr.spawn(function() ldfile('wsconsole.lua')('NODE') end)
+nt = require 'netcon'
+ftr = require 'futures'
+ldfile('wsconsole.lua')()
 require 'helpers'   
 
-ftr.spawn(function() nt.deploy({wifi=true}) end)
-ftr.spawn(function() nt.deploy({ws=false}) end)
+ftr.spawn(function() nt.deploy({wifi=false}) end)
+ftr.spawn(function() nt.deploy({ws=true}) end)
 ftr.spawn(function() nt.deploy({mqtt=true}) end)
 wscon = ldfile('wscon.lua')
 wscon:connect()
@@ -30,3 +32,13 @@ tt = tmr.create()
 tt:alarm(1000, tmr.ALARM_AUTO, function()
 g, r, b = leds.hsv2grb(math.random(0,359), 255, 255);buf:fill(r, g, b);ws2812.write(buf)
 end)
+
+mode('pulse_rainbow')
+clr_timers()
+g, r, b = 100,5,0
+buf:fill(g,r,b);buf:set(1, 0, 0, 0);ws2812.write(buf)
+tt = tmr.create()
+table.insert(timers, tt)
+tt:alarm(40, tmr.ALARM_AUTO, function()buf:shift(1);buf:set(1, 0, 0, 0);buf:set(1, g, r, b);ws2812.write(buf) end)
+cc={0, 12, 25, 100, 150, 230, 280}
+=node.random(1,2)
